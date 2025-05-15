86cb81af631bd49137878a5c99b4e68daa07df86
Zubastic120993 <vladymyrzub@gmail.com>
2025-05-16 00:55:38 +0200

    Add simple homepage to api app

--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from .models import Category, Item
 from .serializers import CategorySerializer, ItemSerializer
 from .permissions import IsOwnerOrReadOnly
+from django.http import HttpResponse
 
 # Create your views here.
 class CategoryViewSet(viewsets.ModelViewSet):
@@ -21,3 +22,6 @@ class ItemViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+def home(request):
+    return HttpResponse("<h1>Welcome to My Django API 🚀</h1><p>Visit <a href='/api/v1/'>the API</a> or <a href='/api/schema/swagger-ui/'>API docs</a>.</p>")
--- core/urls.py
+++ core/urls.py
@@ -18,6 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+from api.views import home
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -25,4 +26,5 @@ urlpatterns = [
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    path("", home, name="home"),
 ]
