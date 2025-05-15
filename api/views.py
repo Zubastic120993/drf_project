a37916257e6e97781cf8b965661b438577c0c7b1
Zubastic120993 <vladymyrzub@gmail.com>
2025-05-15 22:44:12 +0200

    Add views and custom permissions

--- api/views.py
+++ api/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Category, Item
+from .serializers import CategorySerializer, ItemSerializer
+from .permissions import IsOwnerOrReadOnly
 
 # Create your views here.
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['name']
+
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['category']
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
